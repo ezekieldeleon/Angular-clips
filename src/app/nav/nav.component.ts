bb718ff3477d8416975a0f9f4b96c30603e79c8f
Ezekiel <ezekiel.de.leon@jci.com>
2023-01-20 14:40:43 -0800

    Started reactive form for register form.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,16 +4,13 @@ import { ModalService } from '../services/modal.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-
   constructor(public modal: ModalService) {}
 
   openModal($event: Event) {
-    console.log('here');
     $event.preventDefault();
     this.modal.toggleModal('auth');
   }
-
 }
